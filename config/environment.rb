e494e20e96d7319f8b79712752d3be17dad1bf19
Sashweana <sashweana@gmail.com>
2018-06-14 10:21:26 +0800

    added logentries gem and configured LE

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger = Le.new('52bbe88b-a85b-45f5-b5c4-df6d64df8e15', :debug => true, :local => true)
+
+Rails.logger.warn("Look at me, I'm a warning message")
+Rails.logger.info("Look at me, I'm an info message")
+Rails.logger.debug("Look at me, I'm a debug message")
